35a751f585b092f49958ddd49657ac671138ccfc
Raymond Chao <raymond.chao16@gmail.com>
2018-06-12 11:37:01 -0500

    first

---
+++ bash.js
@@ -0,0 +1,22 @@
+const pwd = require('./pwd');
+const ls  = require('./ls');
+
+//Output a prompt
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim(); // remove the newline
+    process.stdout.write('You typed: ' + cmd + '\n');
+
+    if(cmd === 'pwd'){
+        pwd.pwd();
+    }
+
+    else if(cmd === 'ls'){
+        ls.ls();
+    }
+    
+    
+    
+});
---
+++ pwd.js
@@ -0,0 +1,6 @@
+module.exports.pwd = function() {
+    
+    process.stdout.write(process.cwd());
+    
+    process.stdout.write('\nprompt > ');
+}
